e22a4e26fc1127fa50dae987a7931ffc10de589c
mradebowale <hamzwalex@gmail.com>
2023-12-04 11:28:51 +0100

    update

--- CV/settings.py
+++ CV/settings.py
@@ -27,6 +27,7 @@ ALLOWED_HOSTS = ['debowaleolat.com', "landingpa.up.railway.app", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ['https://landingpa.up.railway.app','https://127.0.0.1']
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
 # Application definition
 
 INSTALLED_APPS = [
